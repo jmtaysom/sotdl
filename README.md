2b2a4e0018cf8be4e0254ad6854ca71b4c700fac
jmtaysom <james.taysom@gmail.com>
2018-02-11 15:55:21 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,80 @@
+### Shadow of the Demon Lord Character Generator
+
+This is the very beginning of a character generator for the RPG game
+Shadow of the Demon Lord. The system for creating characters is very
+programmatic and lends itself to automation. 
+
+
+#### Help needed
+The most time consuming part of this project at first will be copying
+the tables out of the book so that characters can be created. There 
+are two main types of tables: those that need a roll of a d20 and have
+20 unique values and those that need a roll of 3d6 and have several 
+ranges of values that correspond to an answer. 
+
+The 3d6 range tables will be represented by two lists. The first will be 
+a breakpoint list and the second will be  a value list. The breakpoint
+list will have one less item than the value list. Consider the following 
+table:
+
+**Human Religion**
+
+|3d6  |Religion|
+|:---:|--------|
+|3|You belong to a cult dedicated to a dark power.|
+|4|You belong to a heretical sect.|
+|5–6 | You were raised in the teachings of witchcraft.|
+|7–10| You follow the tenets of the Old Faith.|
+|11–15| You belong to the Cult of the New God.|
+|16–18|You have no religion.|
+
+The human religion breakpoints are 4, 5, 7, 11, and 16. These are the 
+numbers that separate each group. The list would be written as 
+`human_religion_breakpoints = [4, 5, 7, 11, 16]`. The values would be
+written as 
+```python
+human_religions = [
+'You belong to a cult dedicated to a dark power.',
+'You belong to a heretical sect.',
+'You were raised in the teachings of witchcraft.',
+'You follow the tenets of the Old Faith.',
+'You belong to the Cult of the New God.',
+'You have no religion.'
+]
+```
+
+The d20 tables will be represented by a python dictionary like the 
+human_background dictionary in the human.py file. The die roll will be 
+the key and the result will be the value. The following table would be 
+represented by the following code.
+
+**Human Background**
+
+|3d6|Background|
+|:---:|---|
+|1|You died and returned to life...|
+|2|You were briefly possessed by a demon...|
+|3|You spent {} years as a prisoner in a dungeon.|
+|4|You murdered someone in cold blood...|
+|5|You caught and recovered from a terrible disease.|
+
+```python
+human_background = {
+    1: ("You died and returned to life. You start the game with {} Insanity.", "roll('1d6t')"),
+    2: ("You were briefly possessed by a demon. You start the game with 1 Corruption.", "None"),
+    3: ("You spent {} years as a prisoner in a dungeon.", "roll('1d6t')"),
+    4: ("You murdered someone in cold blood. You start the game with 1 Corruption.", "None"),
+    5: ("You caught and recovered from a terrible disease.", "None"),
+}
+```
+
+Above you will also not that the values of the dictionary are tuples. 
+The first value in the tuple is the string from the table. The second
+is a expression that will be evaluated to give a random number. Many 
+of the tables have a random value such as 1d6 years in prison. So the 
+die roll in the sentence is replaced by `{}` and the expression is 
+written as the value of the second value of the tuple. The roll 
+function takes a string as the input with a die roll like 1d6 written
+as `'1d6t'`. The t at the end tells roll to return the value of the 
+roll instead of a list of the dice rolled.
+ 
---
+++ human.py
@@ -0,0 +1,67 @@
+from bisect import bisect
+from dice import roll
+
+human_background = {
+    1: ("You died and returned to life. You start the game with {} Insanity.", "roll('1d6t')"),
+    2: ("You were briefly possessed by a demon. You start the game with 1 Corruption.", "None"),
+    3: ("You spent {} years as a prisoner in a dungeon.", "roll('1d6t')"),
+    4: ("You murdered someone in cold blood. You start the game with 1 Corruption.", "None"),
+    5: ("You caught and recovered from a terrible disease.", "None"),
+    6: ("You belonged to a strange cult and saw many strange things. You start the game with 1 Insanity.", "None"),
+    7: ("The faerie held you prisoner for {roll('1d20t')} years.", "roll('1d20t')"),
+    8: ("You lost a loved one and their loss haunts you still.", "None"),
+    9: ("You lost a finger, a few teeth, or an ear, or you gained a scar.", "None"),
+    10: ("You earned a living working in your profession.", "None"),
+    11: ("You fell in love and the relationship ended well or is ongoing.", "None"),
+    12: ("You have a spouse and {} children.", "max([roll('1d6-2t'), 0])"),
+    13: ("You traveled extensively. You speak one additional language.", "None"),
+    14: ("You received an education. You know how to read the Common Tongue.", "None"),
+    15: ("You saved your town from terrible monsters.", "None"),
+    16: ("You foiled a plot to kill someone important or you brought a killer to justice.", "None"),
+    17: ("You performed a great deed and are a hero to the people in your hometown.", "None"),
+    18: ("You found an old treasure map.", "None"),
+    19: ("Someone important and powerful owes you a favor.", "None"),
+    20: ("You came into money and start the game with {} cp.", "roll('2d6t')"),
+}
+
+
+def roll_human_background():
+    d20 = roll('1d20t')
+    background, rand = human_background[d20]
+    return background.format(eval(rand))
+
+
+human_personality_breakpoints = [4, 5, 7, 9, 13, 15, 17, 18]
+human_personalities = [
+    "You are cruel, wicked, and self-serving. You enjoy  making others suffer.",
+    "You are erratic and unpredictable. You have a hard time keeping your word and tend toward capricious behavior.",
+    "Might makes right. Obedience to authority is the highest ideal.",
+    "You look after yourself  rst and foremost. You’re not above double-crossing friends.",
+    "You put your interests and those of your friends above all else.",
+    "You help others because it’s the right thing to do.",
+    "You try to do what you think is right, even if it breaks laws and social conventions.",
+    "Your honor and duty guide everything you do.",
+    "You are committed to good and noble causes, and you never stray from your beliefs even if your insistence would " 
+    "cost you your life."
+]
+
+
+def roll_human_personality():
+    dice_roll = roll('3d6t')
+    return human_personalities[bisect(human_personality_breakpoints, dice_roll)]
+
+
+human_religion_breakpoints = [4, 5, 7, 11, 16]
+human_religions = [
+    'You belong to a cult dedicated to a dark power.',
+    'You belong to a heretical sect.',
+    'You were raised in the teachings of witchcraft.',
+    'You follow the tenets of the Old Faith.',
+    'You belong to the Cult of the New God.',
+    'You have no religion.'
+]
+
+
+def roll_human_religion():
+    dice_roll = roll('3d6t')
+    return human_religions[bisect(human_religion_breakpoints, dice_roll)]
