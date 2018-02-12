6dba0195b8424ac943161e409b91b342437757d6
jmtaysom <james.taysom@gmail.com>
2018-02-12 17:13:11 -0500

    added test to check the length of the result is >0

--- tests/test_human.py
+++ tests/test_human.py
@@ -5,14 +5,20 @@ from ancestry import human
 
 @pytest.mark.parametrize("test_input", list(range(3, 19)))
 def test_roll_human_personality(test_input):
-    assert type(human.roll_human_personality(test_input)) == str
+    result = human.roll_human_personality(test_input)
+    assert type(result) == str
+    assert len(result) > 0
 
 
 @pytest.mark.parametrize("test_input", list(range(3, 19)))
 def test_roll_human_religion(test_input):
-    assert type(human.roll_human_religion(test_input)) == str
+    result = human.roll_human_religion(test_input)
+    assert type(result) == str
+    assert len(result) > 0
 
 
 @pytest.mark.parametrize("test_input", list(range(1, 21)))
 def test_roll_human_background(test_input):
-    assert type(human.roll_human_background(test_input)) == str
+    result = human.roll_human_background(test_input)
+    assert type(result) == str
+    assert len(result) > 0
